db892b8f22474f54132ba627008529beac5bef6d
Micah Levine <micah.levine@outlook.com>
2019-12-22 04:19:19 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,6 +4,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
+      t.integer :costume_inventory
+      t.integer :num_of_employees
       
     end
   end
