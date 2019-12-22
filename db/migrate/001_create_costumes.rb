faaa670af361373261de7b0d71c25cabe988f792
Micah Levine <micah.levine@outlook.com>
2019-12-22 03:30:10 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,7 +11,10 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.float :price 
+      t.float :price
+      t.string :image_url
+      t.string :size
+      
     end
   end
 end
