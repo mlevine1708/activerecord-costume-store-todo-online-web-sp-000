bf0c7378605b7388536659b9b12b018659707eb1
Micah Levine <micah.levine@outlook.com>
2019-12-22 02:20:59 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,8 @@
 # Create your haunted_houses migration here
+class HauntedHouse < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+    end
+  end
+end
