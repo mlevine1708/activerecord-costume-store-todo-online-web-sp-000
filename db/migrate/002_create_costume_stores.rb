5ff5ece0bf17e7a9e4c441cf6ac0a81d508f0079
Micah Levine <micah.levine@outlook.com>
2019-12-22 02:20:13 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,8 @@
 # Create your costume_stores migration here
+class costume_stores < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+    end
+  end
+end
