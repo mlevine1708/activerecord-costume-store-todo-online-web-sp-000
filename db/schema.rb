4338f9ecb79137e1be948866b6c5b65142a6f6ad
Micah Levine <micah.levine@outlook.com>
2019-12-22 03:26:31 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 3) do
+
+  create_table "costume_stores", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "costumes", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "haunted_houses", force: :cascade do |t|
+    t.string "name"
+  end
 
 end
