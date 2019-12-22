d76e5ac85da71dcede89e2da6f6b6a6be314fa50
Micah Levine <micah.levine@outlook.com>
2019-12-22 04:14:46 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "costumes", force: :cascade do |t|
     t.string "name"
+    t.float "price"
+    t.string "image_url"
+    t.string "size"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "haunted_houses", force: :cascade do |t|
